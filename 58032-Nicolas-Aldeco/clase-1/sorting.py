a4f7be07424fe336eddc80cbfb179737b9836f8c
Nicolas Aldeco <nico.aldeco@gmail.com>
2019-09-14 16:43:57 -0300

    added merge sort

--- 58032-Nicolas-Aldeco/clase-1/sorting.py
+++ 58032-Nicolas-Aldeco/clase-1/sorting.py
@@ -1,21 +1,50 @@
 class Sort():
 
-    def bubblesort(self,lista):
+    def bubblesort(self, lista):
         n = len(lista)
-        for i in range (1, n):
-            for j in range (n-1):
-                if lista [j] > lista[j+1]:
+        for i in range(1, n):
+            for j in range(n-1):
+                if lista[j] > lista[j+1]:
                     lista[j], lista[j+1] = lista[j+1], lista[j]
         return lista
 
-    def insertionsort(self,listx):
-        n=len(listx)
+    def insertionsort(self, listx):
+        n = len(listx)
         i = 0
-        for i in range(i,n):
-            vol = listx[i] 
+        for i in range(i, n):
+            vol = listx[i]
             j = i
-            while j>0 and listx [j-1]>vol:
-                listx[j]=listx[j-1]
+            while j > 0 and listx[j-1] > vol:
+                listx[j] = listx[j-1]
                 j -= 1
-            listx[j]=vol
+            listx[j] = vol
         return listx
+
+    def mergesort(self, lista):
+        if len(lista) <= 1:
+            return lista
+        middle = int(len(lista) / 2)
+        left = lista[:middle]
+        right = lista[middle:]
+        left = self.mergesort(left)
+        right = self.mergesort(right)
+        return self.merge(left, right)
+
+    def merge(self, listA, listB):
+        new_list = []
+        a = 0
+        b = 0
+        while a < len(listA) and b < len(listB):
+            if listA[a] < listB[b]:
+                new_list.append(listA[a])
+                a += 1
+            else:
+                new_list.append(listB[b])
+                b += 1
+        while a < len(listA):
+            new_list.append(listA[a])
+            a += 1
+        while b < len(listB):
+            new_list.append(listB[b])
+            b += 1
+        return new_list
--- 58032-Nicolas-Aldeco/clase-1/test-sort.py
+++ 58032-Nicolas-Aldeco/clase-1/test-sort.py
@@ -1,30 +1,68 @@
 import unittest
 from sorting import Sort
 
+
 class TestSorting(unittest.TestCase):
 
     def setUp(self):
         self.ls = Sort()
-    def test_BubbleSort_comun(self):
-        lista = [36, 71, 16, 21, 73, 9, 0, 40, 66, 5]
+        self.lista_mixed = [36, 71, 16, 21, 73, 9, 0, 40, 66, 5]
         self.lista_ordenada = [0, 5, 9, 16, 21, 36, 40, 66, 71, 73]
-        ans = self.ls.bubblesort(lista)
-        self.assertEqual(ans,self.lista_ordenada)
-    def test_BubbleSort_ordenada(self):
-        lista = [0, 5, 9, 16, 21, 36, 40, 66, 71, 73]
-        self.lista_ordenada = [0, 5, 9, 16, 21, 36, 40, 66, 71, 73]
-        ans = self.ls.bubblesort(lista)
-        self.assertEqual(ans,self.lista_ordenada)
-    def test_BubbleSort_todos_numeros_iguales(self):
-        lista = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
-        self.lista_ordenada = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
-        ans = self.ls.bubblesort(lista)
-        self.assertEqual(ans,self.lista_ordenada)
-    def test_BubbleSort_con_numeros_negativos(self):
-        lista = [0, 5, 9, 16, 21, -36, -40, 66, 71, 73]
-        self.lista_ordenada = [-40, -36, 0, 5, 9, 16, 21, 66, 71, 73]
-        ans = self.ls.bubblesort(lista)
-        self.assertEqual(ans,self.lista_ordenada)
+        self.lista3 = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+        self.listan = [0, 5, 9, 16, 21, -36, -40, 66, 71, 73]
+        self.lista_ordenadan = [-40, -36, 0, 5, 9, 16, 21, 66, 71, 73]
+# Bubble Sort Testing
+
+    def test_BubbleSort_comun(self):    # 1
+        ans = self.ls.bubblesort(self.lista_mixed)
+        self.assertEqual(ans, self.lista_ordenada)
+
+    def test_BubbleSort_ordenada(self):    # 2
+        ans = self.ls.bubblesort(self.lista_ordenada)
+        self.assertEqual(ans, self.lista_ordenada)
+
+    def test_BubbleSort_todos_numeros_iguales(self):    # 3
+        ans = self.ls.bubblesort(self.lista3)
+        self.assertEqual(ans, self.lista3)
+
+    def test_BubbleSort_con_numeros_negativos(self):    # 4
+        ans = self.ls.bubblesort(self.listan)
+        self.assertEqual(ans, self.lista_ordenadan)
+
+# Insertion Sort Testing
+    def test_InsertionSort_comun(self):    # 5
+        ans = self.ls.insertionsort(self.lista_mixed)
+        self.assertEqual(ans, self.lista_ordenada)
+
+    def test_InsetionSort_ordenada(self):    # 6
+        ans = self.ls.insertionsort(self.lista_ordenada)
+        self.assertEqual(ans, self.lista_ordenada)
+
+    def test_InsertionSort_todos_numeros_iguales(self):    # 7
+        ans = self.ls.insertionsort(self.lista3)
+        self.assertEqual(ans, self.lista3)
+
+    def test_InsetionSort_con_numeros_negativos(self):    # 8
+        ans = self.ls.insertionsort(self.listan)
+        self.assertEqual(ans, self.lista_ordenadan)
+
+# Marge Sort Testing
+    def test_MergeSort_comun(self):     # 9
+        ans = self.ls.mergesort(self.lista_mixed)
+        self.assertEqual(ans, self.lista_ordenada)
+
+    def test_MergeSort_ordenada(self):     # 10
+        ans = self.ls.mergesort(self.lista_ordenada)
+        self.assertEqual(ans, self.lista_ordenada)
+
+    def test_MergeSort_todos_numeros_iguales(self):     # 11
+        ans = self.ls.mergesort(self.lista3)
+        self.assertEqual(ans, self.lista3)
+
+    def test_MergeSort_con_numeros_negativos(self):     # 12
+        ans = self.ls.mergesort(self.listan)
+        self.assertEqual(ans, self.lista_ordenadan)
+
 
 if __name__ == "__main__":
     unittest.main()
